a5cfeb5f6a0d9d838c6e9e3ea7937b312762ec17
amirhosein <amirhosein8292@gmail.com>
2024-01-28 13:29:27 +0330

    edit app.js file

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-function myFunc(){
-  return true;}
-if(true){
-  console.log("hi amir")}else{
-  console.log("bye bye")
+function myFunc() {
+  return true;
+}
+if (true) {
+  console.log("hi amir");
+} else {
+  console.log("bye bye");
+}
+function rejisterFunc(pass) {
+  if (pass === 1234) {
+    return true;
+  } else {
+    alert("siktir");
   }
-function rejisterFunc(){
-return true
 }
